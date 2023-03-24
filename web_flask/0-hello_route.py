74602b8318d75563197cee75faa8a2f56842b737
oladele90 <oladelemiro07@gmail.com>
2023-03-24 08:46:40 -0700

    add documentatio and fix pycode style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""First flask app that handles the home route"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """return Hello HBNB! for / url"""
     return "Hello HBNB!"
 
 if __name__=='__main__':
