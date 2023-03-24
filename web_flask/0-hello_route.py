140f6db715f5c5a518295ea1d4d831b0e6858174
oladele90 <oladelemiro07@gmail.com>
2023-03-24 09:00:51 -0700

    fix pycodestyle by adding blank space around operator and new lines for documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,12 +2,15 @@
 """First flask app that handles the home route"""
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
     """return Hello HBNB! for / url"""
     return "Hello HBNB!"
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
