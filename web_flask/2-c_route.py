6f473595e46195d76b877e5617bafa9feffca96f
oladele90 <oladelemiro07@gmail.com>
2023-03-25 08:33:44 -0700

    adds modules for previous urls / and /hbnb

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,6 +6,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/", strict_slashes=False)
+def hello():
+    """return Hello HBNB! for / url"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """return HBNB for /hbnb url"""
+    return "HBNB"
+
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """return C <text> for /c/<text> url
